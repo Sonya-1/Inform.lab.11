de29c266e8a65350b31d9e06691d3b5d875ec1e8
Smorodskaya Sonya <71211281+Sonya-1@users.noreply.github.com>
2021-04-10 20:27:54 +0300

    del

--- lab11/pathsplit.h
+++
@@ -1,8 +0,0 @@
-#ifndef A_H
-#define A_H
-
-char* substring(char* str, int start, int end);
-void find_ext(char* basename, char** name, char** ext);
-void pathsplit(char* path, char* split[]);
-
-#endif
